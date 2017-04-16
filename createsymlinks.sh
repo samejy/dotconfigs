d1e1d8d75e44e2105f2ebccdcf40087227b4cb8d
james <james_d_byrne@hotmail.com>
2017-04-16 13:05:11 +0100

    Add .xmodmaprc to map caps to ctrl

--- createsymlinks.sh
+++ createsymlinks.sh
@@ -1,6 +1,7 @@
 ln -s $(pwd)/.vimrc ~/.vimrc
-ln -s $(pwd)/.emacs.d/mine ~/.emacs.d/mine
+ln -s $(pwd)/.emacs.d/mine/ ~/.emacs.d/
 ln -s $(pwd)/.emacs.d/init.el ~/.emacs.d/init.el
 ln -s $(pwd)/.stumpwmrc ~/.stumpwmrc
 ln -s $(pwd)/stumpwm/ ~/
+ln -s $(pwd)/.xmodmaprc ~/.xmodmaprc
 
