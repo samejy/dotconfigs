2056162b6371df03323c34e5817b9faef57a59db
James Byrne <james_d_byrne@hotmail.com>
2020-11-10 18:45:49 +0000

    update path to emacsclient

--- createsymlinks.sh
+++ createsymlinks.sh
@@ -6,35 +6,35 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     karabinerdir=~/.config/karabiner
     [ ! -d "$karabinerdir" ] && mkdir -p "$karabinerdir";
-    ln -s $(pwd)/karabiner.json "$karabinerdir"/karabiner.json
+    ln -sf $(pwd)/karabiner.json "$karabinerdir"/karabiner.json
 fi
 
-ln -s $(pwd)/.vimrc ~/.vimrc
-ln -s $(pwd)/.termrc ~/.config/termrc
-ln -s $(pwd)/.emacs.d/mine/ ~/.emacs.d/
-ln -s $(pwd)/.emacs.d/init.el ~/.emacs.d/init.el
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/.ctags ~/.ctags
+ln -sf $(pwd)/.vimrc ~/.vimrc
+ln -sf $(pwd)/.termrc ~/.config/termrc
+ln -sf $(pwd)/.emacs.d/mine/ ~/.emacs.d/
+ln -sf $(pwd)/.emacs.d/init.el ~/.emacs.d/init.el
+ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/.ctags ~/.ctags
 
 if [ "$JB_LOCATION" != "work" ]; then
-    ln -s $(pwd)/.zshrc ~/.zshrc
-    ln -s $(pwd)/.bashrc ~/.bashrc
-    ln -s $(pwd)/.profile ~/.profile
-    ln -s $(pwd)/newsbeuter/ ~/.newsbeuter
-    ln -s $(pwd)/.stumpwmrc ~/.stumpwmrc
-    ln -s $(pwd)/stumpwm/ ~/
-    ln -s $(pwd)/i3-config ~/.config/i3/config
-    ln -s $(pwd)/.xmodmaprc ~/.xmodmaprc
-    ln -s $(pwd)/.Xresources ~/.Xresources
-    ln -s $(pwd)/.startup.sh ~/.startup.sh
+    ln -sf $(pwd)/.zshrc ~/.zshrc
+    ln -sf $(pwd)/.bashrc ~/.bashrc
+    ln -sf $(pwd)/.profile ~/.profile
+    ln -sf $(pwd)/newsbeuter/ ~/.newsbeuter
+    ln -sf $(pwd)/.stumpwmrc ~/.stumpwmrc
+    ln -sf $(pwd)/stumpwm/ ~/
+    ln -sf $(pwd)/i3-config ~/.config/i3/config
+    ln -sf $(pwd)/.xmodmaprc ~/.xmodmaprc
+    ln -sf $(pwd)/.Xresources ~/.Xresources
+    ln -sf $(pwd)/.startup.sh ~/.startup.sh
 
     asdfdir=~/.config/common-lisp/source-registry.conf.d
     if [ ! -d "$asdfdir" ]; then
-	mkdir -p "$asdfdir"
+    	mkdir -p "$asdfdir"
     fi
 
-    ln -s $(pwd)/asdf.conf "$asdfdir"/projects.conf
-    sudo ln -s $(pwd)/enpass /usr/bin/enpass 
+    ln -sf $(pwd)/asdf.conf "$asdfdir"/projects.conf
+    sudo ln -sf $(pwd)/enpass /usr/bin/enpass 
 fi
     
    
